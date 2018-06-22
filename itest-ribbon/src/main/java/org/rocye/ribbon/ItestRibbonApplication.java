02f4b73d507c5ec883c06ee6083c4b2bfe6fbff3
Rocye <hirocye@163.com>
2018-06-22 17:57:36 +0800

    ribbon断路器功能

--- itest-ribbon/src/main/java/org/rocye/ribbon/ItestRibbonApplication.java
+++ itest-ribbon/src/main/java/org/rocye/ribbon/ItestRibbonApplication.java
@@ -1,12 +1,18 @@
 package org.rocye.ribbon;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableHystrix				//加入断路器功能
+@EnableHystrixDashboard		//加入断路器仪表盘功能
 @EnableEurekaClient
 @SpringBootApplication
 public class ItestRibbonApplication {
@@ -20,4 +26,16 @@ public class ItestRibbonApplication {
 	RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
+
+	//断路器仪表盘功能(解决hystrix.stream : 404 : HTTP/1.1 404 )
+	@Bean
+	public ServletRegistrationBean getServlet(){
+		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+		registrationBean.setLoadOnStartup(1);
+		registrationBean.addUrlMappings("/hystrix.stream");
+		registrationBean.setName("HystrixMetricsStreamServlet");
+		return registrationBean;
+	}
+
 }
--- itest-ribbon/src/main/java/org/rocye/ribbon/service/TestService.java
+++ itest-ribbon/src/main/java/org/rocye/ribbon/service/TestService.java
@@ -1,5 +1,6 @@
 package org.rocye.ribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,7 +11,13 @@ public class TestService {
 	@Autowired
 	private RestTemplate restTemplate;
 
+	@HystrixCommand(fallbackMethod = "testError")
 	public String testRibbon(String name) {
 		return restTemplate.getForObject("http://ITEST-CLIENT/test?name=" + name, String.class);
 	}
+
+	public String testError(String name) {
+		return "hello " + name + ", happen error!";
+	}
+
 }
